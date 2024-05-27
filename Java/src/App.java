20dc6643905ee8b21178bf9f926e3a981f1dce19
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-05-27 17:46:29 -0300

    Updates

---
+++ Java/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 1;
+        int b = 2;
+        System.out.println(a+b);
+        System.out.print(add(a,b));
+        
+    }
+    public static int add(int a, int b){
+        return a+b;
+    }
+    
+}
