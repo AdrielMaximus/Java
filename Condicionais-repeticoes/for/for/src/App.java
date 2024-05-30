47b6e29373d5620b873f6c00376806893f9bec12
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-05-30 15:58:47 -0300

    forever

---
+++ Condicionais-repeticoes/for/for/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String padaria[] = {"pao", "doce", "salgado", "torta", "bolo", "pudim", "rosca", "pao de queijo", "pao de batata", "pao de leite", "pao de mel"};
+        for (int objetos=0;objetos<padaria.length ; objetos++) {
+            System.out.println("produtos disponiveis: "+ padaria[objetos]);
+        }
+    }
+}
