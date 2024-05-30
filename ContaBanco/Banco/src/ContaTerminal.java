58be86e50e083806049b20f1180b716d213fa58b
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-05-30 15:13:12 -0300

    goal

--- ContaBanco/Banco/src/ContaTerminal.java
+++ ContaBanco/Banco/src/ContaTerminal.java
@@ -6,6 +6,16 @@ public class ContaTerminal {
         double saldo= 0.0;
         String Nome = null;
         String agencia = null;
-        System.out.print(Nome + agencia + numeros + saldo);
+        System.out.print("Digite o nome do titular da conta: ");
+        Nome = sc.nextLine();
+        System.out.print("Digite o número da conta EX:(xxxx): ");
+        numeros = sc.nextInt();
+        System.out.print("Digite o número da agência EX:(xxxx): ");
+        agencia = sc.next();
+        System.out.print("Digite o saldo da conta EX:(0,00): ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + Nome +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numeros + " e seu saldo" + saldo + " já está disponível para saque");
+        
     }
 }
