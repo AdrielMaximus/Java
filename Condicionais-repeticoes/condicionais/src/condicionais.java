d2c23377373d52f183490bf76584af8ad0a7b1df
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-05-30 15:47:13 -0300

    if

--- Condicionais-repeticoes/condicionais/src/condicionais.java
+++ Condicionais-repeticoes/condicionais/src/condicionais.java
@@ -1,5 +1,19 @@
-public class App {
+import java.util.Scanner;
+
+public class condicionais {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sua divida atual é de 200 reais. qual o valor que voce possui para sanar a sua divida");
+        int saldo = scanner.nextInt();
+        int divida = 200;
+        int valor = saldo - divida;
+        if(saldo == divida){
+            System.out.println("Divida quitada");
+        } if(saldo < divida){
+            
+            System.err.println("falta" + valor +" Reais");
+
+        }
+        scanner.close();
     }
 }
