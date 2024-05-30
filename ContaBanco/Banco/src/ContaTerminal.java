87be29ad74669e70a547c6a91e89117e6c29a5a3
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-05-30 14:52:13 -0300

    add scanner

--- ContaBanco/Banco/src/ContaTerminal.java
+++ ContaBanco/Banco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int numeros = 0;
+        double saldo= 0.0;
+        String Nome = null;
+        String agencia = null;
+        System.out.print(Nome + agencia + numeros + saldo);
     }
 }
