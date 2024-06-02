62c82ced88d10a8d9a431733f23ac2149d4acd8f
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-06-02 09:41:59 -0300

    catch

--- Condicionais-repeticoes/for/for/src/App.java
+++ Condicionais-repeticoes/for/for/src/App.java
@@ -1,8 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String padaria[] = {"pao", "doce", "salgado", "torta", "bolo", "pudim", "rosca", "pao de queijo", "pao de batata", "pao de leite", "pao de mel"};
-        for (int objetos=0;objetos<padaria.length ; objetos++) {
-            System.out.println("produtos disponiveis: "+ padaria[objetos]);
+        try {
+            for (int objetos=0;objetos<padaria.length ; objetos++) {
+                System.out.println("produtos disponiveis: "+ padaria[objetos]);
+            }
+        } catch (Exception e) {
+            System.out.println("Erro: "+ e.getMessage());
         }
     }
 }
