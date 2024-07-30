2df4e3650e6e1d174047ca5ac6f34f9965c03723
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-07-30 15:16:37 -0300

    leraning oop

--- OOP/Desafio/src/App.java
+++ OOP/Desafio/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    
+        public static void main(String[] args) throws Exception {
+        /*public = + free access
+         * private = - just for the class itself
+         * protected = # just for the package and subclasses
+         */
         System.out.println("Hellook World!");
     }
 }
