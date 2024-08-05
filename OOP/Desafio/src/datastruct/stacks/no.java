8971dcb36478e7a4bc1b8b0d76599bda1b3946dd
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-08-05 17:14:11 -0300

    Update no.java

--- OOP/Desafio/src/datastruct/stacks/no.java
+++ OOP/Desafio/src/datastruct/stacks/no.java
@@ -1,14 +1,31 @@
-package datastruct.stacks;
-public class no {
-    public static void main(String[] args) {
-        private int dado;
-        private No reno = null;
-        
-        public No(int dado) {
-            this.dado = dado;
-        }
-        public No(){
 
-        }
+package datastruct.stacks;
+
+public class No {
+    private int dado;
+    private No reno = null;
+
+    public No(int dado) {
+        this.dado = dado;
     }
-}
+
+    public No() {
+    }
+
+    // Getter for dado
+    public int getDado() {
+        return dado;
+    }
+
+    // Setter for dado
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        No no = new No();
+        no.setDado(10);
+        System.out.println(no.getDado());
+    }
+}        
