037b690616e3910d150c46bd21d6eb047bb1e7f6
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-08-05 17:00:30 -0300

    a

--- OOP/Desafio/src/datastruct/stacks/app.java
+++ OOP/Desafio/src/datastruct/stacks/app.java
@@ -1,4 +1,4 @@
-package datastruct;
+package datastruct.stacks;
 public class app {
     public static void main (String[] args) {
         
---
+++ OOP/Desafio/src/datastruct/stacks/no.java
@@ -0,0 +1,14 @@
+package datastruct.stacks;
+public class no {
+    public static void main(String[] args) {
+        private int dado;
+        private No reno = null;
+        
+        public No(int dado) {
+            this.dado = dado;
+        }
+        public No(){
+
+        }
+    }
+}
