a38f40bbf9fb38e7e3a6530751a1343bbf742890
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-08-12 16:25:57 -0300

    stack

--- OOP/Desafio/src/datastruct/stacks/app.java
+++
@@ -1,6 +0,0 @@
-package datastruct.stacks;
-public class app {
-    public static void main (String[] args) {
-        
-    }
-}
--- OOP/Desafio/src/datastruct/stacks/no.java
+++ OOP/Desafio/src/datastruct/stacks/no.java
@@ -1,31 +1,35 @@
-
 package datastruct.stacks;
-
 public class No {
     private int dado;
-    private No reno = null;
+    private No refNo = null;
+
+    public No() {
+    }
 
     public No(int dado) {
         this.dado = dado;
     }
 
-    public No() {
-    }
-
-    // Getter for dado
     public int getDado() {
         return dado;
     }
 
-    // Setter for dado
     public void setDado(int dado) {
         this.dado = dado;
     }
 
-    public static void main(String[] args) {
-        // Example usage
-        No no = new No();
-        no.setDado(10);
-        System.out.println(no.getDado());
+    public No getRefNo() {
+        return refNo;
     }
-}        
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "dado=" + dado +
+                '}';
+    }
+}
