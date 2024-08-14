68da45c7816e89e8d0f663a5ee2f5f98baa8879d
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-08-14 16:36:38 -0300

    this code have 6 errors, but still work

--- Projects/Cache/cache/src/main/java/com/project/cache/repository/ProductRepository.java
+++ Projects/Cache/cache/src/main/java/com/project/cache/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.project.cache.repository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 @Repository
+@Slf4j
 public class ProductRepository {
     public String listAll() {
-
+        log.info("Request to list all products");
         return "List of products";
     }
 }
--- Projects/Cache/cache/src/main/java/com/project/cache/service/ProductService.java
+++ Projects/Cache/cache/src/main/java/com/project/cache/service/ProductService.java
@@ -2,12 +2,14 @@ package com.project.cache.service;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import com.project.cache.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
     public String listAll() {
-
+        log.info("Request to list all products");
         return "List of products";
     }
 }
